f07c20e586dfbde5028ec04e3b6b57f6e46a8f17
jissai <antapala@gmail.com>
2013-03-08 15:22:03 +0100

    TinyPG callable from command line
    *add some unit test

--- TinyPG.UnitTests/ProgramTester.cs
+++ TinyPG.UnitTests/ProgramTester.cs
@@ -51,13 +51,29 @@ namespace TinyPG.UnitTests
         {
            StringBuilder input =  new StringBuilder(string.Empty)
                 .Append(@"<% @TinyPG %>")
-                .Append(@"Start -> .*;");
+                .Append("ALL -> @\".*\";")
+                .Append("Start -> ALL;");
 
-            Program prog = new Program(ManageError, new StringBuilder(string.Empty));
+            Program prog = new Program(FailError, new StringBuilder(string.Empty));
             Grammar grammar = prog.ParseGrammar(input.ToString(), "");
             Assert.IsNotNull(grammar);
         }
 
-        private void ManageError(ParseTree tree, StringBuilder output) { }
+        [TestMethod]
+        public void TestParseGrammarNOK()
+        {
+            StringBuilder input = new StringBuilder(string.Empty)
+                 .Append(@"<% @TinyPG %>")
+                 .Append("ALL -> @\".*Start -> ALL;");
+
+            Program prog = new Program(PassError, new StringBuilder(string.Empty));
+            Grammar grammar = prog.ParseGrammar(input.ToString(), "");
+            Assert.IsNull(grammar);
+        }
+
+        private void FailError(ParseTree tree, StringBuilder output) { Assert.Fail(); }
+        private void PassError(ParseTree tree, StringBuilder output) { Assert.IsTrue(tree.Errors.Count > 0); 
+        
+        }
     }
 }
