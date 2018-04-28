a1af2cd39874284e08eb8094d3d12713ace17963
Ultrapingu <juliensoler@yahoo.fr>
2018-04-28 15:53:33 +0200

    reindent

--- TinyPG.UnitTests/ParserTester.cs
+++ TinyPG.UnitTests/ParserTester.cs
@@ -7,230 +7,230 @@ using TinyPG.Compiler;
 
 namespace TinyPG.UnitTests
 {
-    /// <summary>
-    /// Summary description for UnitTest1
-    /// </summary>
-    [TestClass]
-    public class ParserTester
-    {
-        // TODO: set the correct paths to be able to run the unittests succesfully
-        private const string TEMPLATEPATH = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG\Templates\C#\";
-        private const string TEMPLATEPATH_VB = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG\Templates\VB\";
-        private const string OUTPUTPATH = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG.UnitTests\";
-        private const string TESTFILESPATH = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG.UnitTests\Testfiles\";
+	/// <summary>
+	/// Summary description for UnitTest1
+	/// </summary>
+	[TestClass]
+	public class ParserTester
+	{
+		// TODO: set the correct paths to be able to run the unittests succesfully
+		private const string TEMPLATEPATH = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG\Templates\C#\";
+		private const string TEMPLATEPATH_VB = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG\Templates\VB\";
+		private const string OUTPUTPATH = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG.UnitTests\";
+		private const string TESTFILESPATH = @"D:\MyProjects\Net\TinyPG v1.3\TinyPG.UnitTests\Testfiles\";
 
-        public ParserTester()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
+		public ParserTester()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
 
-        private TestContext testContextInstance;
+		private TestContext testContextInstance;
 
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
-
-        private GrammarTree LoadGrammar(string filename)
-        {
-            string grammarfile = System.IO.File.ReadAllText(filename);
-            Scanner scanner = new Scanner();
-            Parser parser = new Parser(scanner);
-            GrammarTree tree = (GrammarTree)parser.Parse(grammarfile, filename, new GrammarTree());
-            return tree;
-        }
-
-        [TestMethod]
-        public void SimpleExpression1_Test()
-        {
-            
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression1.tpg");
-            Grammar G = (Grammar) GT.Eval();
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
 
 
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
-            G.Directives["TinyPG"]["OutputPath"] = OUTPUTPATH;
+		private GrammarTree LoadGrammar(string filename)
+		{
+			string grammarfile = System.IO.File.ReadAllText(filename);
+			Scanner scanner = new Scanner();
+			Parser parser = new Parser(scanner);
+			GrammarTree tree = (GrammarTree)parser.Parse(grammarfile, filename, new GrammarTree());
+			return tree;
+		}
 
-            // basic checks
-            string temp = G.PrintFirsts();
-            Assert.IsTrue(!String.IsNullOrEmpty(temp));
-            temp = G.GetOutputPath();
-            Assert.IsTrue(!String.IsNullOrEmpty(temp));
-            temp = G.PrintGrammar();
-            Assert.IsTrue(!String.IsNullOrEmpty(temp));
+		[TestMethod]
+		public void SimpleExpression1_Test()
+		{
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
-            
-            compiler.Compile(G);
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression1.tpg");
+			Grammar G = (Grammar)GT.Eval();
 
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            CompilerResult result = compiler.Run("5+7/3*2+(4*2)");
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
+			G.Directives["TinyPG"]["OutputPath"] = OUTPUTPATH;
 
-            
+			// basic checks
+			string temp = G.PrintFirsts();
+			Assert.IsTrue(!String.IsNullOrEmpty(temp));
+			temp = G.GetOutputPath();
+			Assert.IsTrue(!String.IsNullOrEmpty(temp));
+			temp = G.PrintGrammar();
+			Assert.IsTrue(!String.IsNullOrEmpty(temp));
 
-            Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
-        }
+			Compiler.Compiler compiler = new Compiler.Compiler();
 
-        [TestMethod]
-        public void SimpleExpression1_VB_Test()
-        {
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression1_vb.tpg");
-            Grammar G = (Grammar)GT.Eval();
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
+			compiler.Compile(G);
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            compiler.Compile(G);
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+			CompilerResult result = compiler.Run("5+7/3*2+(4*2)");
 
-            CompilerResult result = compiler.Run("5+7/3*2+(4*2)");
 
-            Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
-        }
 
-        [TestMethod]
-        public void SimpleExpression2_Test()
-        {
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression2.tpg");
-            Grammar G = (Grammar)GT.Eval();
-            G.Directives.Add(new Directive("TinyPG"));
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
+			Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
+		}
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
+		[TestMethod]
+		public void SimpleExpression1_VB_Test()
+		{
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression1_vb.tpg");
+			Grammar G = (Grammar)GT.Eval();
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
 
-            compiler.Compile(G);
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+			Compiler.Compiler compiler = new Compiler.Compiler();
 
-            CompilerResult result = compiler.Run("5+8/4*2+(4*2)");
+			compiler.Compile(G);
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            Assert.IsTrue(Convert.ToInt32(result.Value) == 17);
-        }
+			CompilerResult result = compiler.Run("5+7/3*2+(4*2)");
 
-        [TestMethod]
-        public void SimpleExpression2_VB_Test()
-        {
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression2_vb.tpg");
-            Grammar G = (Grammar)GT.Eval();
-            G.Directives.Add(new Directive("TinyPG"));
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
+			Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
+		}
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
+		[TestMethod]
+		public void SimpleExpression2_Test()
+		{
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression2.tpg");
+			Grammar G = (Grammar)GT.Eval();
+			G.Directives.Add(new Directive("TinyPG"));
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
 
-            compiler.Compile(G);
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+			Compiler.Compiler compiler = new Compiler.Compiler();
 
-            CompilerResult result = compiler.Run("5+8/4*2+(4*2)");
+			compiler.Compile(G);
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            Assert.IsTrue(Convert.ToInt32(result.Value) == 17);
-        }
+			CompilerResult result = compiler.Run("5+8/4*2+(4*2)");
 
-        [TestMethod]
-        public void SimpleExpression3_Test()
-        {
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"BNFGrammar v1.1.tpg");
-            Grammar G = (Grammar)GT.Eval();
-            G.Directives.Add(new Directive("TinyPG"));
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
+			Assert.IsTrue(Convert.ToInt32(result.Value) == 17);
+		}
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
+		[TestMethod]
+		public void SimpleExpression2_VB_Test()
+		{
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"simple expression2_vb.tpg");
+			Grammar G = (Grammar)GT.Eval();
+			G.Directives.Add(new Directive("TinyPG"));
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
 
-            compiler.Compile(G);
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+			Compiler.Compiler compiler = new Compiler.Compiler();
 
-            CompilerResult result = compiler.Run("");
+			compiler.Compile(G);
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
-        }
+			CompilerResult result = compiler.Run("5+8/4*2+(4*2)");
 
-        [TestMethod]
-        public void SimpleExpression3_VB_Test()
-        {
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"BNFGrammar_vb v1.1.tpg");
-            Grammar G = (Grammar)GT.Eval();
-            G.Directives.Add(new Directive("TinyPG"));
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
+			Assert.IsTrue(Convert.ToInt32(result.Value) == 17);
+		}
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
+		[TestMethod]
+		public void SimpleExpression3_Test()
+		{
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"BNFGrammar v1.1.tpg");
+			Grammar G = (Grammar)GT.Eval();
+			G.Directives.Add(new Directive("TinyPG"));
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
 
-            compiler.Compile(G);
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+			Compiler.Compiler compiler = new Compiler.Compiler();
 
-            CompilerResult result = compiler.Run("");
+			compiler.Compile(G);
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
-        }
+			CompilerResult result = compiler.Run("");
 
-        [TestMethod]
-        public void SimpleExpression4_Test()
-        {
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"GrammarHighlighter.tpg");
-            Grammar G = (Grammar)GT.Eval();
-            G.Directives.Add(new Directive("TinyPG"));
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
+			Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
+		}
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
+		[TestMethod]
+		public void SimpleExpression3_VB_Test()
+		{
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"BNFGrammar_vb v1.1.tpg");
+			Grammar G = (Grammar)GT.Eval();
+			G.Directives.Add(new Directive("TinyPG"));
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
 
-            compiler.Compile(G);
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+			Compiler.Compiler compiler = new Compiler.Compiler();
 
-            CompilerResult result = compiler.Run("using System.IO;\r\n");
+			compiler.Compile(G);
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
-        }
+			CompilerResult result = compiler.Run("");
 
-        [TestMethod]
-        public void SimpleExpression4_VB_Test()
-        {
-            GrammarTree GT = LoadGrammar(TESTFILESPATH + @"GrammarHighlighter_vb.tpg");
-            Grammar G = (Grammar)GT.Eval();
-            G.Directives.Add(new Directive("TinyPG"));
-            G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
+			Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
+		}
 
-            Compiler.Compiler compiler = new Compiler.Compiler();
+		[TestMethod]
+		public void SimpleExpression4_Test()
+		{
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"GrammarHighlighter.tpg");
+			Grammar G = (Grammar)GT.Eval();
+			G.Directives.Add(new Directive("TinyPG"));
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH;
 
-            compiler.Compile(G);
-            Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+			Compiler.Compiler compiler = new Compiler.Compiler();
 
-            CompilerResult result = compiler.Run("using System.IO;\r\n");
+			compiler.Compile(G);
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
 
-            Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
-        }
-    }
+			CompilerResult result = compiler.Run("using System.IO;\r\n");
+
+			Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
+		}
+
+		[TestMethod]
+		public void SimpleExpression4_VB_Test()
+		{
+			GrammarTree GT = LoadGrammar(TESTFILESPATH + @"GrammarHighlighter_vb.tpg");
+			Grammar G = (Grammar)GT.Eval();
+			G.Directives.Add(new Directive("TinyPG"));
+			G.Directives["TinyPG"]["TemplatePath"] = TEMPLATEPATH_VB;
+
+			Compiler.Compiler compiler = new Compiler.Compiler();
+
+			compiler.Compile(G);
+			Assert.IsTrue(compiler.Errors.Count == 0, "compilation contains errors");
+
+			CompilerResult result = compiler.Run("using System.IO;\r\n");
+
+			Assert.IsTrue(result.Output.StartsWith("Parse was successful."));
+		}
+	}
 }
--- TinyPG.UnitTests/ProgramTester.cs
+++ TinyPG.UnitTests/ProgramTester.cs
@@ -7,83 +7,85 @@ using TinyPG.Compiler;
 
 namespace TinyPG.UnitTests
 {
-    /// <summary>
-    /// This class provides support for testing Program.cs.
-    /// It covers public Methods and Main()
-    /// </summary>
-    [TestClass]
-    public class ProgramTester
-    {
-        public ProgramTester()
-        {
+	/// <summary>
+	/// This class provides support for testing Program.cs.
+	/// It covers public Methods and Main()
+	/// </summary>
+	[TestClass]
+	public class ProgramTester
+	{
+		public ProgramTester()
+		{
 
-        }
+		}
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-        
-        /// <summary>
-        /// Test command line parameter, when file doesn't exist.
-        /// </summary>
-        [TestMethod]
-        public void TestFileNOK()
-        {
-            Assert.AreEqual((int)Program.ExitCode.InvalidFilename, Program.Main(new string[] { "foo" }));
-        }
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
 
-        /// <summary>
-        /// Testing grammar parsing when grammar is OK.
-        /// </summary>
-        [TestMethod]
-        public void TestParseGrammar()
-        {
-           StringBuilder input =  new StringBuilder(string.Empty)
-                .Append(@"<% @TinyPG %>")
-                .Append("ALL -> @\".*\";")
-                .Append("Start -> ALL;");
+		/// <summary>
+		/// Test command line parameter, when file doesn't exist.
+		/// </summary>
+		[TestMethod]
+		public void TestFileNOK()
+		{
+			Assert.AreEqual((int)Program.ExitCode.InvalidFilename, Program.Main(new string[] { "foo" }));
+		}
 
-            Program prog = new Program(FailError, new StringBuilder(string.Empty));
-            Grammar grammar = prog.ParseGrammar(input.ToString(), "");
-            Assert.IsNotNull(grammar);
-        }
+		/// <summary>
+		/// Testing grammar parsing when grammar is OK.
+		/// </summary>
+		[TestMethod]
+		public void TestParseGrammar()
+		{
+			StringBuilder input = new StringBuilder(string.Empty)
+				 .Append(@"<% @TinyPG %>")
+				 .Append("ALL -> @\".*\";")
+				 .Append("Start -> ALL;");
 
-        /// <summary>
-        /// Testing grammar parsing when grammar is NOT OK.
-        /// </summary>
-        [TestMethod]
-        public void TestParseGrammarNOK()
-        {
-            StringBuilder input = new StringBuilder(string.Empty)
-                 .Append(@"<% @TinyPG %>")
-                 .Append("ALL -> @\".*Start -> ALL;");
+			Program prog = new Program(FailError, new StringBuilder(string.Empty));
+			Grammar grammar = prog.ParseGrammar(input.ToString(), "");
+			Assert.IsNotNull(grammar);
+		}
 
-            Program prog = new Program(PassError, new StringBuilder(string.Empty));
-            Grammar grammar = prog.ParseGrammar(input.ToString(), "");
-            Assert.IsNull(grammar);
-        }
+		/// <summary>
+		/// Testing grammar parsing when grammar is NOT OK.
+		/// </summary>
+		[TestMethod]
+		public void TestParseGrammarNOK()
+		{
+			StringBuilder input = new StringBuilder(string.Empty)
+				 .Append(@"<% @TinyPG %>")
+				 .Append("ALL -> @\".*Start -> ALL;");
 
-        private void FailError(ParseTree tree, StringBuilder output) { Assert.Fail(); }
-        private void PassError(ParseTree tree, StringBuilder output) { Assert.IsTrue(tree.Errors.Count > 0); 
-        
-        }
-    }
+			Program prog = new Program(PassError, new StringBuilder(string.Empty));
+			Grammar grammar = prog.ParseGrammar(input.ToString(), "");
+			Assert.IsNull(grammar);
+		}
+
+		private void FailError(ParseTree tree, StringBuilder output) { Assert.Fail(); }
+		private void PassError(ParseTree tree, StringBuilder output)
+		{
+			Assert.IsTrue(tree.Errors.Count > 0);
+
+		}
+	}
 }
